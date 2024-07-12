ef7d1f0fb53087402418709bd24271f52d7f4ea0
NguyenNgocHaiLuan <nguyenngochailuan16112003@gmail.com>
2024-07-12 23:22:20 +0700

    Add files via upload

---
+++ webpt/main.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+
+
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("trained_model.h5")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(64,64))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) 
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions)
+
+
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page",["Home","About Project","Prediction"])
+
+if(app_mode=="Home"):
+    st.header("FRUITS & VEGETABLES RECOGNITION SYSTEM")
+    image_path = "fruitsandvetegables.jpg"
+    st.image(image_path)
+
+elif(app_mode=="About Project"):
+    st.header("About Project")
+    st.subheader("About Dataset")
+    st.text("This dataset contains images of the following food items:")
+    st.code("fruits- banana, apple, pear, grapes, orange, kiwi, watermelon, pomegranate, pineapple, mango.")
+    st.code("vegetables- cucumber, carrot, capsicum, onion, potato, lemon, tomato, raddish, beetroot, cabbage, lettuce, spinach, soy bean, cauliflower, bell pepper, chilli pepper, turnip, corn, sweetcorn, sweet potato, paprika, jalepeño, ginger, garlic, peas, eggplant.")
+    st.subheader("Content")
+    st.text("This dataset contains three folders:")
+    st.text("1. train (100 images each)")
+    st.text("2. test (10 images each)")
+    st.text("3. validation (10 images each)")
+
+elif(app_mode=="Prediction"):
+    st.header("Model Prediction")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    if(st.button("Predict")):
+        st.snow()
+        st.write("Our Prediction")
+        result_index = model_prediction(test_image)
+        #Reading Labels
+        with open("labels.txt") as f:
+            content = f.readlines()
+        label = []
+        for i in content:
+            label.append(i[:-1])
+        st.success("Model is Predicting it's a {}".format(label[result_index]))
